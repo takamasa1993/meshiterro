10ada25c65bce0649a5792d01e4229c9cd85bad4
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-04-15 11:05:51 +0000

    [finish4-12]

--- app/helpers/post_images_helper.rb
+++ app/helpers/post_images_helper.rb
@@ -1,2 +1,3 @@
-module PostimagesHelper
+module PostImagesHelper
+  # ヘルパーメソッドなど
 end
--- test/controllers/post_images_controller_test.rb
+++ test/controllers/post_images_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
-class PostimagesControllerTest < ActionDispatch::IntegrationTest
+class PostImagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  
 end
