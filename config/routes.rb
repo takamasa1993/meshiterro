e758159ad591a8c405342538272614a7b7bcea4b
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-04-18 10:55:45 +0000

    [finish4-18]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   devise_for :users
-
- resources :post_images, only: [:new, :create, :index, :destroy, :show]
-
+ 
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   get 'homes/about', to: 'homes#about', as: 'about'
   resources :users, only: [:show, :edit, :update]
 end
